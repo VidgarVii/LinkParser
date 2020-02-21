c0677e37b666353833ab2f43e3305eb371a4b303
VidaspVii <vidasp@mail.ru>
2020-02-22 02:32:56 +0300

    Add require all file rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 require_relative '../lib/link_parser'
 
 Bundler.require :default, LinkParser.env
+Dir[LinkParser.root.join('config', '**', '*.rb')].each { |f| require f }
 
 LinkParser.application.bootstrap!
