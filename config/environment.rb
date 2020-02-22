2cf0e99dfa2aceb18dc4c5e8b88b16a566a3a64d
VidaspVii <vidasp@mail.ru>
2020-02-22 03:11:53 +0300

    Include files for models and controllers

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,7 @@ require_relative '../lib/link_parser'
 
 Bundler.require :default, LinkParser.env
 Dir[LinkParser.root.join('config', '**', '*.rb')].each { |f| require f }
+Dir[LinkParser.root.join('app', 'models', '*.rb')].each { |f| require f }
+Dir[LinkParser.root.join('app', 'controllers', '*.rb')].each { |f| require f }
 
 LinkParser.application.bootstrap!
