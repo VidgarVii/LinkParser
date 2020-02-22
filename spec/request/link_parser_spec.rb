311d283a0a17ecff0bce6cbeaf6a53ecee7b5e92
VidaspVii <vidasp@mail.ru>
2020-02-23 02:12:02 +0300

    Add request tests

---
+++ spec/request/link_parser_spec.rb
@@ -0,0 +1,12 @@
+describe 'LinkParser::Application', type: :request do
+  include_context 'shared_env'
+
+  subject { LinkParser::Application.instance }
+  it 'should return status 404 if not valid router' do
+    expect(subject.call(shared_env).first).to eq 404
+  end
+
+  it 'should return status 200 if valid router' do
+    expect(subject.call(shared_valid_env).first).to eq 200
+  end
+end
