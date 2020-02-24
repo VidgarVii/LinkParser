5bb73f22dc2bce5f26d8a61075fcf46369476e9a
VidaspVii <vidasp@mail.ru>
2020-02-24 12:18:24 +0300

    Add test for SiteFactory#create. (async mode off)

--- spec/factories/site_factory_spec.rb
+++ spec/factories/site_factory_spec.rb
@@ -17,4 +17,20 @@ describe LinkParser::SiteFactory do
       end
     end
   end
+
+  describe '#create' do
+    it 'objects site should write to the db' do
+      expect { subject.create }.to change(LinkParser::Site, :count).by(2)
+    end
+
+    it 'should return 2 objects' do
+      expect(subject.create.size).to eq 2
+    end
+
+    it 'return data should contain public attrs' do
+      %i(status title url).each do |attr|
+        expect(subject.create.first.has_key?(attr)).to be_truthy
+      end
+    end
+  end
 end
